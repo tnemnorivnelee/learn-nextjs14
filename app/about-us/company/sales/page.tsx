a42aaee48cd6c16ffb065bbde20730bb4c214390
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-03-21 21:24:54 +0900

    feat: defining routes

---
+++ app/about-us/company/sales/page.tsx
@@ -0,0 +1,7 @@
+export default function Sales() {
+  return (
+    <>
+      <h1>SALES</h1>
+    </>
+  );
+}
---
+++ app/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+  return (
+    <>
+      <h1>ABOUTUS</h1>
+    </>
+  );
+}
