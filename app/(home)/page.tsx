5a59b3af03824d116bbb196f4f5a99316661fcc3
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-03-23 21:41:57 +0900

    feat: client side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,32 @@
-export const metadata = {
-  title: 'Home',
-};
+'use client';
+
+import { useEffect, useState } from 'react';
+
+// export const metadata = {
+//   title: 'Home',
+// };
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>HELLO</h1>
+      {isLoading ? 'Loading...' : JSON.stringify(movies)}
     </div>
   );
 }
