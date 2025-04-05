de6e4a1f7849295838601d2e376fe993c3595325
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-04-05 23:43:07 +0900

    feat: Error handling

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+export default function ErrorOMG() {
+  return (
+    <div>
+      <h1>OMG</h1>
+      <h2>Something went wrong</h2>
+      <h3>Try again later</h3>
+      <h4>Or check the console</h4>
+      <h5>Or check the network tab</h5>
+    </div>
+  );
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,8 +3,9 @@ import { API_URL } from '../app/(home)/page';
 async function getVideos(id: string) {
   console.log(`Fetching movie : ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  throw new Error('Error fetching videos');
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
